98918f317a9e9ce445ce2901f1b8768ec37adcda
almirkopic <almirkopic1992@gmail.com>
2024-09-23 09:41:28 -0500

    new changes on routes

--- packages/node-server/dist/server.js
+++ packages/node-server/dist/server.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const users_1 = __importDefault(require("./src/routes/users")); // Importuj rute
+const users_1 = __importDefault(require("./src/routes/users"));
 const app = (0, express_1.default)();
 const port = 3001;
 app.use((0, cors_1.default)());
